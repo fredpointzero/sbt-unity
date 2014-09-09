3474d6a98804845790631cc2bf0f1343df150d99
fredericvauchelles <fredericvauchelles@.(none)>
2014-09-09 18:23:08 +0200

    Update: (wip) unity build

--- src/main/scala/com/mindwaves-studio/sbt-unity/UnityPlugin.scala
+++ src/main/scala/com/mindwaves-studio/sbt-unity/UnityPlugin.scala
@@ -4,6 +4,7 @@ import java.nio.file.Files
 
 import sbt._
 import sbt.Keys._
+import sbt.inc.Analysis
 
 /**
  * Created by fredericvauchelles on 07/09/2014.
@@ -14,6 +15,7 @@ object UnityPlugin extends sbt.Plugin{
   object UnityKeys {
     val unityEditorExecutable = SettingKey[File]("unity-editor-executable", "Path to the Unity editor executable to use")
     val unitySourceDirectories = SettingKey[Seq[(String, File)]]("unity-source-directories", "Default Unity source directories")
+    val unityBuildTarget = SettingKey[UnityWrapper.UnityBuildTarget.Value]("unity-build-target", "Target platform for the build")
     val generateWorkspace = TaskKey[File]("generate-workspace", "Generate a Unity workspace")
   }
 
@@ -60,7 +62,15 @@ object UnityPlugin extends sbt.Plugin{
       unitySourceDirectories in Compile := Seq(
         ("main", (sourceDirectory in Compile).value / "runtime_resources")
       ),
-      generateWorkspace in Compile <<= generateWorkspaceTaskById("Build", Compile)
+      generateWorkspace in Compile <<= generateWorkspaceTaskById("Build", Compile),
+      compile in Compile <<= (generateWorkspace in Compile, unityBuildTarget in Compile, target) map { (generatedWorkspaceDir, buildTarget, targetDir) => {
+        val targetDirectory = targetDir / s"build_${buildTarget}_main";
+        if(!targetDirectory.exists()) {
+          targetDirectory.mkdirs();
+        }
+        UnityWrapper.buildUnityPlayer(generatedWorkspaceDir, targetDir / s"build_${buildTarget}_main.log", buildTarget, targetDirectory);
+        Analysis.Empty
+      }}
     )) ++
     inConfig(Test)(unitySettings0 ++ Seq(
       unitySourceDirectories in Test := Seq(
@@ -71,6 +81,7 @@ object UnityPlugin extends sbt.Plugin{
     ))
 
   private def unitySettings0: Seq[Setting[_]] = Seq(
-    unityEditorExecutable := UnityWrapper.detectUnityExecutable
+    unityEditorExecutable := UnityWrapper.detectUnityExecutable,
+    unityBuildTarget := UnityWrapper.getBuildTargetCapabilitiesFromOS(System.getProperty("os.name"))(0)
   );
 }
--- src/main/scala/com/mindwaves-studio/sbt-unity/UnityWrapper.scala
+++ src/main/scala/com/mindwaves-studio/sbt-unity/UnityWrapper.scala
@@ -8,8 +8,10 @@ import sbt._
 object UnityWrapper {
   import scala.sys.process._
 
-  private val WindowsPattern = "(.*win.*)".r;
-  private val OSXPattern = "(.*mac.*)".r;
+  object UnityBuildTarget extends Enumeration {
+    type UnityBuildTarget = Value;
+    val Windows, Windows64, OSX, OSX64, OSXUniversal, Linux32, Linux64, LinuxUniversal, Web, WebStreamed, None = Value;
+  }
 
   val UNITY_EXECUTABLE_SYSTEM_PROPERTY = "UNITY_EDITOR_PATH";
 
@@ -38,6 +40,14 @@ object UnityWrapper {
     }
   }
 
+  def getBuildTargetCapabilitiesFromOS(osName:String):Seq[UnityBuildTarget.Value] = {
+    osName toLowerCase match {
+      case WindowsPattern(c) => Seq(UnityBuildTarget.Windows, UnityBuildTarget.Windows64, UnityBuildTarget.Web, UnityBuildTarget.WebStreamed);
+      case OSXPattern(c) => Seq(UnityBuildTarget.OSX, UnityBuildTarget.OSX,  UnityBuildTarget.OSXUniversal, UnityBuildTarget.Web, UnityBuildTarget.WebStreamed);
+      case _ => Seq(UnityBuildTarget.None);
+    }
+  }
+
   def createUnityProjectAt(projectPath:File, logFile:File) = {
     val result = Seq(
       detectUnityExecutable.getAbsolutePath(),
@@ -52,4 +62,43 @@ object UnityWrapper {
       throw new RuntimeException(s"Could not create Unity project at $projectPath (see $logFile)");
     }
   }
+
+  def buildUnityPlayer(projectPath: File, logFile: File, targetPlatform:UnityBuildTarget.Value, targetDirectory:File) = {
+    val buildCapabilities = getBuildTargetCapabilitiesFromOS(System.getProperty("os.name"));
+    if (!buildCapabilities.contains(targetPlatform)) {
+      throw new IllegalArgumentException(s"Target platform $targetPlatform is not supported on this OS");
+    }
+
+    val buildMethod = targetPlatform match {
+      case UnityBuildTarget.Linux32 => "buildLinux32Player";
+      case UnityBuildTarget.Linux64 => "buildLinux64Player";
+      case UnityBuildTarget.LinuxUniversal => "buildLinuxUniversalPlayer";
+      case UnityBuildTarget.OSX => "buildOSXPlayer";
+      case UnityBuildTarget.OSX64 => "buildOSX64Player";
+      case UnityBuildTarget.OSXUniversal => "buildOSXUniversalPlayer";
+      case UnityBuildTarget.Web => "buildWebPlayer";
+      case UnityBuildTarget.WebStreamed => "buildWebPlayerStreamed";
+      case UnityBuildTarget.Windows => "buildWindowsPlayer";
+      case UnityBuildTarget.Windows64 => "buildWindows64Player";
+      case _ => throw new IllegalArgumentException(s"Unmanaged build target: $targetPlatform");
+    }
+
+    val result = Seq(
+      detectUnityExecutable.getAbsolutePath(),
+      "-batchMode",
+      "-quit",
+      "-logFile",
+      logFile.getAbsolutePath(),
+      "-projectPath ",
+      projectPath.getAbsolutePath(),
+      s"-$buildMethod",
+      targetDirectory.getAbsolutePath()).!;
+
+    if(result != 0) {
+      throw new RuntimeException(s"Could not build player Unity project at $projectPath (see $logFile)");
+    }
+  }
+
+  private val WindowsPattern = "(.*win.*)".r;
+  private val OSXPattern = "(.*mac.*)".r;
 }
